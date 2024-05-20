c6ec53ba7b506a8230c332e1a21699c0d0fcfd59
Mauro Ciro <112350847+MauroCiro0812@users.noreply.github.com>
2024-05-20 11:02:08 -0500

    Microservice

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from microservice import microservice
 
 app = Flask(__name__)
 
-@app.route('/Microservice')
+@app.route('/Microservice/', methods=['GET'])
 def microservice_init():
-    microservice.ejecucion()
-    return "ejecuto"
+    nit = request.args.get('nit')
+    mes = request.args.get('mes')
+    anio = request.args.get('anio')
+    
+    if nit is None and mes is None and anio is None:
+        return jsonify({'error': 'Se requieren los tres parámetros: id, mes y año'}), 400
+    
+    print(nit, mes, anio)
+    json_file = microservice.ejecucion(nit, mes, anio)
+    return json_file
 
 if __name__=="__main__":
     app.run(debug=True)
--- microservice.py
+++ microservice.py
@@ -1,8 +1,54 @@
-from post import postgres
+import psycopg2
+import json
 
-class microservice(postgres):
 
-    def ejecucion():
-        postgres.conexion_db()
-        #postgres.query_db()
-        #postgres.cerrar_db
+class microservice():
+
+    def ejecucion(nit, mes, anio):
+        try:
+            connection = psycopg2.connect(
+                host = 'localhost',
+                user = 'postgres',
+                password = 'toor',
+                database = 'pruebabd',
+                port = '5432'
+            )
+            print("Conexion Exitosa")
+        except Exception as ex:
+            print(ex)
+
+        cur = connection.cursor()
+        datos = []
+        if mes == 0 or mes == "":
+                mes = None
+    
+        if anio == 0 or anio == "":
+            anio = None
+            
+        if nit == 0 or nit == "":
+            nit = None
+        
+        if nit and not mes and not anio:
+        # Consultar basada solo en el NIT
+            cur.execute(f"SELECT nombre, descripcion FROM empresas WHERE nit = {nit}")
+            for nombre, descripcion in cur.fetchall():
+                datos.append({'nombre': nombre, 'descripcion': descripcion})
+
+        elif not nit and mes and anio:
+            # Consultar basada solo en mes y anio
+            cur.execute(f"SELECT nombre, descripcion FROM empresas WHERE mes = {mes} AND anio = {anio}")
+            for nombre, descripcion in cur.fetchall():
+                datos.append({'nombre': nombre, 'descripcion': descripcion})
+
+        elif nit and mes and anio:
+            # Consultar basada en todos los parámetros
+            cur.execute(f"SELECT nombre, descripcion FROM empresas WHERE nit = {nit} AND mes = {mes} AND anio = {anio}")
+            for nombre, descripcion in cur.fetchall():
+                datos.append({'nombre': nombre, 'descripcion': descripcion})
+
+        else:
+            # Si no hay suficientes datos para la consulta
+            print("No se proporcionaron suficientes datos para la consulta")
+        
+        return json.dumps(datos)
+        connection.close()
